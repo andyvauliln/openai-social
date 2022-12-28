aa93101ccb6c8b63523c2a33291bc333cc3296eb
andyvauliln <andy.vaulin@gmail.com>
2022-12-28 16:27:40 +0800

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,4 @@
-"use client";
-
 import "../styles/globals.css";
-import { createTheme } from "@mui/material";
-import { ThemeProvider } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    action: {
-      disabledBackground: "red",
-      disabled: "white",
-      
-    },
-  },
-});
 
 export default function RootLayout({
   children,
@@ -22,9 +8,7 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body className="bg-black">
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </body>
+      <body className="">{children}</body>
     </html>
   );
 }
